cfdb36103ae218bad34b6b945fd82f740c4274b6
DmytroShumylo <153100385+DmytroShumylo@users.noreply.github.com>
2024-01-14 11:28:52 +0200

    task 2

--- js/lessons-1.js
+++ js/lessons-1.js
@@ -1,8 +1,16 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-
+/*
 const name = "ГАЛИНА";
 const age = 35;
 const hobby = "JAVASCRIPT";
 const total = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`;
-console.log(total);
+console.log(total);*/
+
+/*ЗАДАЧА 2
+ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"*/
+const name = prompt("введіть ім'я");
+const email = prompt("ваша пошта");
+const phone = prompt("ваш телефон");
+const message = `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ  ${email} ПОЧТУ І ${phone} ТЕЛЕФОН`
+alert(message);
