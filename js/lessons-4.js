94536455be651c83d2ce79d0e80859df220c7aa1
DimaKarvackij <dimakarvackij777@gmail.com>
2024-02-25 11:10:19 +0200

    commit

--- js/lessons-4.js
+++ js/lessons-4.js
@@ -7,23 +7,58 @@
 // Добавте метод getInfo(), який повертає рядок:
 // User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
 
-class User {
-  constructor({ name, age, numberOfPosts }) {
-    this.name = name;
-    this.age = age;
-    this.numberOfPosts = numberOfPosts;
-  }
+// class User {
+//   constructor({ name, age, numberOfPosts }) {
+//     this.name = name;
+//     this.age = age;
+//     this.numberOfPosts = numberOfPosts;
+//   }
 
-  getInfo() {
-    return `User ${this.name} is ${this.age} years old and has ${this.numberOfPosts} posts.`;
+//   getInfo() {
+//     return `User ${this.name} is ${this.age} years old and has ${this.numberOfPosts} posts.`;
+//   }
+// }
+
+// const mango = new User({
+//   name: "mango",
+//   age: 25,
+//   numberOfPosts: 4,
+// });
+
+// console.log(mango);
+// console.log(mango.getInfo());
+
+// ЗАДАЧА 2
+// Напиши клас Storage який створює об'єкти для керування складом товарів.
+// При виклику отримуватиме один аргумент - початковий масив товарів, і записуватиме його у властивість items.
+// Добавте методи класу:
+
+// getItems() - повертає масив товарів.
+// addItem(item) - отримує новий товар та додає його до поточних.
+// removeItem(item) - отримає товар і, якщо він є, видаляє його з поточних.
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    const result = this.items.indexOf(item);
+    if (result === -1) {
+      return;
+    } else {
+      this.items.splice(result, 1);
+    }
   }
 }
 
-const mango = new User({
-  name: "mango",
-  age: 25,
-  numberOfPosts: 4,
-});
+const items = new Storage(["Виноград", "Банани", "Кокоси", "Ківі"]);
+items.addItem("Манго");
+items.removeItem("Ківі");
 
-console.log(mango);
-console.log(mango.getInfo());
+console.log(items);
