bc8ef1fcc0409f067260d59911303d143b73df6c
DimaKarvackij <dimakarvackij777@gmail.com>
2024-03-24 13:21:55 +0200

    commit 8

--- js/lessons-6.js
+++ js/lessons-6.js
@@ -9,61 +9,102 @@
 // https://api.thecatapi.com/v1/images/search?breed_ids=ідентифікатор_породи
 // Напиши функцію fetchCatByBreed(breedId), яка очікує ідентифікатор породи, робить HTTP-запит і повертає проміс із даними про кота - результатом запиту.
 
-const breedSelect = document.querySelector(".breed-select");
-const catInfo = document.querySelector(".cat-info");
+// const breedSelect = document.querySelector(".breed-select");
+// const catInfo = document.querySelector(".cat-info");
 
-const API_KEY =
-  "live_y5a4tkdGBJQpeay6ebguCY0DjbxDWxkHUH8lNyNftVHs40xXTrJEHqM1N34t3PmP ";
+// const API_KEY =
+//   "live_y5a4tkdGBJQpeay6ebguCY0DjbxDWxkHUH8lNyNftVHs40xXTrJEHqM1N34t3PmP ";
 
-const API_URL = "https://api.thecatapi.com/v1";
+// const API_URL = "https://api.thecatapi.com/v1";
 
-function fetchBreeds() {
-  return fetch(`${API_URL}/breeds?api_key=${API_KEY}`).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+// function fetchBreeds() {
+//   return fetch(`${API_URL}/breeds?api_key=${API_KEY}`).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
 
 // console.log(fetchBreeds());
 
-function fetchCatByBreed(breedId) {
-  return fetch(
-    `${API_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+// function fetchCatByBreed(breedId) {
+//   return fetch(
+//     `${API_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+//   ).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
 
-fetchBreeds()
-  .then((date) => (breedSelect.innerHTML = mur(date)))
-  .catch((error) => console.error(error));
+// fetchBreeds()
+//   .then((date) => (breedSelect.innerHTML = mur(date)))
+//   .catch((error) => console.error(error));
 
-function mur(array) {
-  const resolt = array.map(
-    ({ name, id }) =>
-      `<option value="${id}" data-placeholder="true">${name}</option>`
-  );
-  return resolt.join("");
-}
+// function mur(array) {
+//   const resolt = array.map(
+//     ({ name, id }) =>
+//       `<option value="${id}" data-placeholder="true">${name}</option>`
+//   );
+//   return resolt.join("");
+// }
 
-function imgGalery(ivent) {
-  const breedId = ivent.target.value;
-  //   console.log(breedId);
-  fetchCatByBreed(breedId)
-    .then((catOk) => {
-      catInfo.innerHTML = createMarckup(catOk);
+// function imgGalery(ivent) {
+//   const breedId = ivent.target.value;
+//   console.log(breedId);
+//   fetchCatByBreed(breedId)
+//     .then((catOk) => {
+//       catInfo.innerHTML = createMarckup(catOk);
+//     })
+//     .catch((catError) => console.error(catError));
+// }
+
+// breedSelect.addEventListener("change", imgGalery);
+
+// function createMarckup(cat) {
+//   const { url } = cat[0];
+//   return `<img src="${url}" width="500" alt="Cat">`;
+// }
+// ЗАДАЧА 2
+// ВИКОРИСТОВУЮЧИ API ДАНОГО ПОСИЛАННЯ  https://www.thecocktaildb.com/api.php , ТА ЗА ДОПОМОГОЮ fetch, ВИВОДЬ РЕЗУЛЬТАТ НА ЕКРАН.
+// РЕЗУЛЬТАТ ПОШУКУ МОЖНА ОТРИМАТИ ІЗ ІМПУТА, КОЛИ КЛІЄНТ НАБРАВ ДАНИЙ ТОВАР(ВИКОРИСТАЙ РОЗМІТКУ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА)
+
+//www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+const forData = document.querySelector("#form");
+const inputData = document.querySelector("#input");
+const containerData = document.querySelector(".container");
+forData.addEventListener("click", formData);
+
+function formData(event) {
+  event.preventDefault();
+  const result = inputData.value;
+  console.log(result);
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${result}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
     })
-    .catch((catError) => console.error(catError));
+    .then((date) => createMarckup(date.drinks))
+    .catch((eror) => console.log(eror));
 }
 
-breedSelect.addEventListener("change", imgGalery);
-
-function createMarckup(cat) {
-  const { url } = cat[0];
-  return `<img src="${url}" width="500" alt="Cat">`;
+function mapArr({ strDrinkThumb, strGlass }) {
+  const textData = `<h2>${strGlass}</h2>
+  <img src="${strDrinkThumb}"/>`;
+  containerData.insertAdjacentHTML("beforeend", textData);
 }
+
+function createMarckup(arr) {
+  arr.forEach((elent) => mapArr(elent));
+}
+// function arrMap(arr) {
+//   return arr.forEach((element) => mapArr(element));
+// }
+
+//strDrinkThumb
+
+//strGlass
